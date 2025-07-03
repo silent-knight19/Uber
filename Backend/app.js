005c66aefbbd21e94c77c4bc1518c1b48a0e89a7
silent-knight19 <sachinsinghtomar7749@gmail.com>
2025-07-03 18:02:51 +0530

    basic setup

---
+++ Backend/app.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+    
